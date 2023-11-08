c37c17ed9fae46684a151996c90b660c26038f47
dominicpam89 <dominicpam89@gmail.com>
2023-11-08 22:36:15 +0700

    prettier Auth/Login.tsx and Auth/Register.tsx

--- src/features/Auth/Login.tsx
+++ src/features/Auth/Login.tsx
@@ -1,93 +1,78 @@
-import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import { useFormik } from 'formik';
-import { AuthLoginInitialValues, AuthLoginValidationSchema } from "../../utils/Auth/login";
-import {
-  TextField,
-  FormControl,
-  InputLabel,
-  OutlinedInput,
-  InputAdornment,
-  IconButton,
-  FormHelperText,
-} from "@mui/material";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
-
-
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useFormik } from "formik"
+import { AuthLoginInitialValues, AuthLoginValidationSchema } from "../../utils/Auth/login"
+import { TextField, FormControl, InputLabel, OutlinedInput, InputAdornment, IconButton, FormHelperText } from "@mui/material"
+import Visibility from "@mui/icons-material/Visibility"
+import VisibilityOff from "@mui/icons-material/VisibilityOff"
 
 const AuthLogin = () => {
-  const [showPassword, setShowPassword] = useState<boolean>(false);
-  const formik = useFormik({
-    initialValues: AuthLoginInitialValues,
-    validationSchema: AuthLoginValidationSchema,
-    onSubmit:values=>{
-      console.log(values);
-    }
-  });
-  const navigate = useNavigate();
-  return (
-    <>
-      <form
-        className="flex flex-col space-y-8"
-        onSubmit={(e) => {
-          e.preventDefault();
-          formik.handleSubmit();
-        }}
-      >
-        <TextField aria-label="input-username"
-          error={
-            formik.errors.username && formik.touched.username ? true : false
-          }
-          helperText={(formik.errors.username && formik.touched.username) ? formik.errors.username:null}
-          id="username"
-          label="Username"
-          {...formik.getFieldProps("username")}
-        />
-        <FormControl variant="outlined" aria-label="input-password" error={(formik.errors.password && formik.touched.password) ? true:false}>
-          <InputLabel htmlFor="password">
-            Password
-          </InputLabel>
-          <OutlinedInput
-            id="password"
-            type={showPassword ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={() => setShowPassword((ps) => !ps)}
-                  onMouseDown={() => setShowPassword((ps) => !ps)}
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+    const formik = useFormik({
+        initialValues: AuthLoginInitialValues,
+        validationSchema: AuthLoginValidationSchema,
+        onSubmit: (values) => {
+            console.log(values)
+        },
+    })
+    const navigate = useNavigate()
+    return (
+        <>
+            <form
+                className="flex flex-col space-y-4"
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    formik.handleSubmit()
+                }}
+            >
+                <TextField
+                    aria-label="input-username"
+                    error={formik.errors.username && formik.touched.username ? true : false}
+                    helperText={formik.errors.username && formik.touched.username ? formik.errors.username : null}
+                    id="username"
+                    label="Username"
+                    {...formik.getFieldProps("username")}
+                />
+                <FormControl variant="outlined" aria-label="input-password" error={formik.errors.password && formik.touched.password ? true : false}>
+                    <InputLabel htmlFor="password">Password</InputLabel>
+                    <OutlinedInput
+                        id="password"
+                        type={showPassword ? "text" : "password"}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton aria-label="toggle password visibility" onClick={() => setShowPassword((ps) => !ps)} onMouseDown={() => setShowPassword((ps) => !ps)}>
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="password"
+                        {...formik.getFieldProps("password")}
+                    />
+                    {formik.errors.password && formik.touched.password && <FormHelperText error>{formik.errors.password}</FormHelperText>}
+                </FormControl>
+
+                <div className="w-full flex space-x-2" aria-label="input-button-group">
+                    <button className="w-full p-3 btn-primary-outlined" type="reset" onClick={formik.handleReset}>
+                        Reset
+                    </button>
+                    <button className="w-full p-3 btn-primary" type="submit">
+                        Submit
+                    </button>
+                </div>
+
+                <button
+                    aria-label="navigate-login"
+                    onClick={(e) => {
+                        e.preventDefault()
+                        navigate("/auth?mode=register")
+                    }}
+                    className="w-full p-3 rounded-lg font-semibold text-primary-500"
                 >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="password"
-            {...formik.getFieldProps('password')}
-          />
-          {(formik.errors.password && formik.touched.password) && <FormHelperText error>{formik.errors.password}</FormHelperText>}
-        </FormControl>
+                    Register new account
+                </button>
+            </form>
+        </>
+    )
+}
 
-        <div className="w-full flex space-x-2" aria-label="input-button-group">
-          <button className="w-full p-3 btn-primary-outlined" type="reset" onClick={formik.handleReset}>
-            Reset
-          </button>
-          <button className="w-full p-3 btn-primary" type="submit">
-            Submit
-          </button>
-        </div>
-        <button aria-label="navigate-login"
-          onClick={(e) => {
-            e.preventDefault();
-            navigate("/auth?mode=register");
-          }}
-          className="w-full p-3 rounded-lg font-semibold text-primary-500"  
-        >
-          Register new account
-        </button>
-      </form>
-    </>
-  );
-};
-
-export default AuthLogin;
+export default AuthLogin
--- src/features/Auth/Register.tsx
+++ src/features/Auth/Register.tsx
@@ -1,113 +1,93 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import {
-  FormControl,
-  IconButton,
-  OutlinedInput,
-  InputAdornment,
-  InputLabel,
-  TextField,
-  FormHelperText,
-} from "@mui/material";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import { useFormik } from "formik";
-import {
-  AuthRegisterValidationSchema,
-  AuthRegisterInitialValues,
-} from "../../utils/Auth/register";
-
-
-
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { FormControl, IconButton, OutlinedInput, InputAdornment, InputLabel, TextField, FormHelperText } from "@mui/material"
+import Visibility from "@mui/icons-material/Visibility"
+import VisibilityOff from "@mui/icons-material/VisibilityOff"
+import { useFormik } from "formik"
+import { AuthRegisterValidationSchema, AuthRegisterInitialValues } from "../../utils/Auth/register"
 
 const AuthRegister = () => {
-  const [showPassword, setShowPassword] = useState<boolean>(false);
-  const navigate = useNavigate();
-  const formik = useFormik({
-    initialValues: AuthRegisterInitialValues,
-    validationSchema: AuthRegisterValidationSchema,
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
-  
-  return (
-    <>
-      <form
-        className="flex flex-col space-y-4"
-        onSubmit={(e) => {
-          e.preventDefault();
-          formik.handleSubmit();
-        }}
-      >
-        <TextField aria-label="input-first-name"
-          error={
-            formik.errors.firstName && formik.touched.firstName ? true : false
-          }
-          helperText={(formik.errors.firstName && formik.touched.firstName) ? formik.errors.firstName:null}
-          id="firstName"
-          label="First Name"
-          {...formik.getFieldProps("firstName")}
-        />
-        <TextField aria-label="input-last-name"
-          error={
-            formik.errors.lastName && formik.touched.lastName ? true : false
-          }
-          helperText={(formik.errors.lastName && formik.touched.lastName) ? formik.errors.lastName:null}
-          id="lastName"
-          label="Last Name"
-          {...formik.getFieldProps("lastName")}
-        />
-        <TextField aria-label="input-username"
-          error={
-            formik.errors.username && formik.touched.username ? true : false
-          }
-          helperText={(formik.errors.username && formik.touched.username) ? formik.errors.username:null}
-          id="username"
-          label="Username"
-          {...formik.getFieldProps("username")}
-        />
-        <FormControl variant="outlined" aria-label="input-password" error={(formik.errors.password && formik.touched.password) ? true:false}>
-          <InputLabel htmlFor="password">
-            Password
-          </InputLabel>
-          <OutlinedInput
-            id="password"
-            type={showPassword ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={() => setShowPassword((ps) => !ps)}
-                  onMouseDown={() => setShowPassword((ps) => !ps)}
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="password"
-            {...formik.getFieldProps('password')}
-          />
-          {(formik.errors.password && formik.touched.password) && <FormHelperText error>{formik.errors.password}</FormHelperText>}
-        </FormControl>
-        <div id="button-group" className="w-full flex space-x-2">
-          <button className="w-full p-3 btn-primary-outlined" type="reset" onClick={formik.handleReset}>Reset</button>
-          <button className="w-full p-3 btn-primary" type="submit">
-            Submit
-          </button>
-        </div>
-        <button id="navigate-register-button"
-          onClick={(e) => {
-            e.preventDefault();
-            navigate("/auth?mode=login");
-          }}
-          className="w-full p-3 rounded-lg font-semibold text-primary-500"
-        >
-          Already has account?
-        </button>
-      </form>
-    </>
-  );
-};
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+    const navigate = useNavigate()
+    const formik = useFormik({
+        initialValues: AuthRegisterInitialValues,
+        validationSchema: AuthRegisterValidationSchema,
+        onSubmit: (values) => {
+            console.log(values)
+        },
+    })
 
-export default AuthRegister;
+    return (
+        <>
+            <form
+                className="flex flex-col space-y-4"
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    formik.handleSubmit()
+                }}
+            >
+                <TextField
+                    aria-label="input-first-name"
+                    error={formik.errors.firstName && formik.touched.firstName ? true : false}
+                    helperText={formik.errors.firstName && formik.touched.firstName ? formik.errors.firstName : null}
+                    id="firstName"
+                    label="First Name"
+                    {...formik.getFieldProps("firstName")}
+                />
+                <TextField
+                    aria-label="input-last-name"
+                    error={formik.errors.lastName && formik.touched.lastName ? true : false}
+                    helperText={formik.errors.lastName && formik.touched.lastName ? formik.errors.lastName : null}
+                    id="lastName"
+                    label="Last Name"
+                    {...formik.getFieldProps("lastName")}
+                />
+                <TextField
+                    aria-label="input-username"
+                    error={formik.errors.username && formik.touched.username ? true : false}
+                    helperText={formik.errors.username && formik.touched.username ? formik.errors.username : null}
+                    id="username"
+                    label="Username"
+                    {...formik.getFieldProps("username")}
+                />
+                <FormControl variant="outlined" aria-label="input-password" error={formik.errors.password && formik.touched.password ? true : false}>
+                    <InputLabel htmlFor="password">Password</InputLabel>
+                    <OutlinedInput
+                        id="password"
+                        type={showPassword ? "text" : "password"}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton aria-label="toggle password visibility" onClick={() => setShowPassword((ps) => !ps)} onMouseDown={() => setShowPassword((ps) => !ps)}>
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="password"
+                        {...formik.getFieldProps("password")}
+                    />
+                    {formik.errors.password && formik.touched.password && <FormHelperText error>{formik.errors.password}</FormHelperText>}
+                </FormControl>
+                <div id="button-group" className="w-full flex space-x-2">
+                    <button className="w-full p-3 btn-primary-outlined" type="reset" onClick={formik.handleReset}>
+                        Reset
+                    </button>
+                    <button className="w-full p-3 btn-primary" type="submit">
+                        Submit
+                    </button>
+                </div>
+                <button
+                    id="navigate-register-button"
+                    onClick={(e) => {
+                        e.preventDefault()
+                        navigate("/auth?mode=login")
+                    }}
+                    className="w-full p-3 rounded-lg font-semibold text-primary-500"
+                >
+                    Already has account?
+                </button>
+            </form>
+        </>
+    )
+}
+
+export default AuthRegister
