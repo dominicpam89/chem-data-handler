4107cb15b6b017ccd514a3d27f2aed80230f8d2b
dominicpam89 <dominicpam89@gmail.com>
2024-02-05 02:49:19 +0700

    added Query Type to useGetCompounds.ts

--- src/data/hooks/useGetCompounds.ts
+++ src/data/hooks/useGetCompounds.ts
@@ -1,8 +1,10 @@
 import { useQuery } from "@tanstack/react-query"
 import { getCompounds } from "../services/compound"
+import { TypeCompound } from "../context/compound"
+import { ErrorFetch } from "../services/errors"
 
 export const useGetCompounds = ()=>{
-  const {data,error,isLoading,isError,isSuccess} = useQuery({
+  const {data,error,isLoading,isError,isSuccess} = useQuery<TypeCompound[], ErrorFetch, TypeCompound[], string[]>({
     queryKey: ["compounds"],
     queryFn: getCompounds
   })
