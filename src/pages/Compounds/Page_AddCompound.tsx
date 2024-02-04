8c6f8ce6dfe1a6a88f9a78a3eccc0625e397978a
dominicpam89 <dominicpam89@gmail.com>
2024-02-05 03:37:24 +0700

    added temporary Page_AddCompound.tsx

--- src/pages/Compounds/Page_AddCompound.tsx
+++ src/pages/Compounds/Page_AddCompound.tsx
@@ -1,7 +1,20 @@
+import { Button, Stack, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 const AddCompound = () => {
-  return <>
-    Add Compound
-  </>
+  const navigate = useNavigate()
+  return (
+		<Stack
+			direction="column"
+			spacing={2}
+			padding={4}
+			justifyContent="center"
+			alignItems="center"
+		>
+      <Typography variant="h3" component="h1">Wait for Pubchem API</Typography>
+      <Button variant="contained" onClick={()=>navigate("/compounds")}>Back to Compounds</Button>
+    </Stack>
+  )
 }
  
 export default AddCompound;
