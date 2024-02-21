f9079ba06b0d8b473a6eab1553cbe9a8487d51f3
dominicpam89 <dominicpam89@gmail.com>
2024-02-21 16:03:50 +0700

    added Vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,34 @@
+const vitest = require("eslint-plugin-vitest")
+
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+	global: { ...vitest.default.environments.env.globals },
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended-type-checked",
+		"plugin:@typescript-eslint/stylistic-type-checked",
+		"plugin:react-hooks/recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:vitest/recommended",
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		project: ["./tsconfig.json", "./tsconfig.node.json"],
+		tsconfigRootDir: __dirname,
+	},
+	plugins: ["react-refresh"],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+		"no-unused-vars": "warn", // warning, not error
+		"vitest/expect-expect": "off", // eliminate distracting red squiggles while writing tests
+		"react/prop-types": "off", // turn off props validation
+	},
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    // vite config
-    plugins: [react()],
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
-    },
-    optimizeDeps:{
-      include:['@emotion/styled']
-    }
-  }
+	// Load env file based on `mode` in the current working directory.
+	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+	const env = loadEnv(mode, process.cwd(), "")
+	return {
+		// vite config
+		plugins: [react()],
+		define: {
+			__APP_ENV__: JSON.stringify(env.APP_ENV),
+		},
+		optimizeDeps: {
+			include: ["@emotion/styled"],
+		},
+		test: {
+			globals: true,
+			environment: "jsdom",
+			setupFiles: "./src/setupVitest.ts",
+			css: false,
+		},
+	}
 })
