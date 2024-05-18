ebb3e9af7922337bff7579104a3a784e4d722257
dominicpam89 <dominicpam89@gmail.com>
2024-05-18 14:09:24 +0700

    minor fix on some issues before deployment

--- src/pages/CompoundAdd/styled.ts
+++ src/pages/CompoundAdd/styled.ts
@@ -1,33 +1,29 @@
-import { Box, InputLabel as InputLabelBase, styled } from "@mui/material"
+import { Box, InputLabel as InputLabelBase, styled } from '@mui/material';
 
-export const Container = styled(Box)(({theme})=>({
+export const Container = styled(Box)(({ theme }) => ({
 	maxWidth: theme.breakpoints.values.md,
-	width: "100%",
-	display: "flex",
-	flexDirection: "column",
+	width: '100%',
+	display: 'flex',
+	flexDirection: 'column',
 	gap: theme.spacing(4),
-}))
+}));
 
-export const PubChemContainer = styled(Box)(({theme})=>({
-	width: "100%",
-	display: "flex",
-	flexDirection: "column",
+export const PubChemContainer = styled(Box)(({ theme }) => ({
+	width: '100%',
+	display: 'flex',
+	flexDirection: 'column',
 	gap: theme.spacing(4),
-}))
+}));
 
-export const Form = styled("form")(({theme})=>({
-	width: "100%",
-	display: "flex",
-	flexDirection: "column",
+export const Form = styled('form')(({ theme }) => ({
+	width: '100%',
+	display: 'flex',
+	flexDirection: 'column',
 	gap: theme.spacing(2),
-}))
+}));
 
-export const InputLabel = styled(InputLabelBase)(({theme})=>({
+export const InputLabel = styled(InputLabelBase)(({ theme }) => ({
 	backgroundColor: theme.palette.background.default,
-	paddingLeft: "8px",
-	paddingRight: "8px",
-}))
-
-export const FormGroup = styled("div")(({theme})=>({
-	
-}))
+	paddingLeft: '8px',
+	paddingRight: '8px',
+}));
