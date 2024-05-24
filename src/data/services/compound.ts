ca7d9e68463b89047faac81feb837fe481cded9e
dominicpam89 <dominicpam89@gmail.com>
2024-05-24 10:25:22 +0700

    adjusted URL to point to API Gateway / middleware server

--- src/data/services/compound.ts
+++ src/data/services/compound.ts
@@ -1,9 +1,13 @@
+/**   
+	URL is API Gateway or middleware
+	that created for specifically this chem-handler-app
+*/
 const URL = 'https://chem-handler-api-v2.vercel.app';
 import { TypeCompound } from '../context/compound';
 import { getError } from './errors';
 
 export const getCompounds = async () => {
-	const response = await fetch(URL + '/compounds');
+	const response = await fetch(URL + '/compounds', { mode: 'cors' });
 	if (!response.ok) {
 		const error = getError(response);
 		throw error;
@@ -13,12 +17,11 @@ export const getCompounds = async () => {
 };
 
 export const getCompound = async (id: string) => {
-	const response = await fetch(URL + '/compounds?pk=' + id);
+	const response = await fetch(URL + '/compounds/' + id, { mode: 'cors' });
 	if (!response.ok) {
 		const error = getError(response);
 		throw error;
 	}
-	const dataArr = await response.json();
-	const data: TypeCompound = await dataArr[0];
+	const data = await response.json();
 	return data;
 };
