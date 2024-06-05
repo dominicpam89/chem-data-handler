3bf14fc64238940853e22fb73d00542d49622157
dominicpam89 <dominicpam89@gmail.com>
2024-06-05 15:10:45 +0700

    added animation and robust loading image in ImageComponent.tsx

--- src/pages/SearchResult/ImageComponent.tsx
+++ src/pages/SearchResult/ImageComponent.tsx
@@ -1,18 +1,34 @@
-import { Box, styled } from "@mui/material";
+import { Box, CircularProgress, styled } from "@mui/material";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
-const Image = styled("img")(({}) => ({
-	width: 240,
-	height: 240,
-	borderRadius: 999,
+const ImageElement = styled("img")(({}) => ({
+	width: "100%",
+	height: "100%",
 }));
 
 const ImageComponent: React.FC<{ pictureUrl: string; alt: string }> = ({
 	pictureUrl,
 	alt,
 }) => {
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		const img = new Image();
+		img.src = pictureUrl;
+		img.onload = () => setLoading(false);
+	}, [pictureUrl]);
 	return (
 		<Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
-			<Image src={pictureUrl} alt={alt} />
+			{loading && <CircularProgress />}
+			{!loading && (
+				<motion.div
+					style={{ borderRadius: 999, overflow: "hidden" }}
+					initial={{ width: 0, height: 0, rotateZ: -180 }}
+					animate={{ width: 240, height: 240, rotateZ: 0 }}
+				>
+					<ImageElement src={pictureUrl} alt={alt} />
+				</motion.div>
+			)}
 		</Box>
 	);
 };
