0fcb6e27c9523a8594758a83fab6a37abb22c6da
dominicpam89 <dominicpam89@gmail.com>
2024-06-10 12:57:08 +0700

    integrate standalone static ketcher into main app

--- src/pages/KetcherPage.tsx
+++ src/pages/KetcherPage.tsx
@@ -1,5 +1,26 @@
+import { Stack, Typography, Link } from "@mui/material";
+
 const PageKetcher = () => {
-	return <>Page Ketcher</>;
+	return (
+		<Stack width="100%" height="100vh" direction="column" gap={6}>
+			<Stack direction="column" gap={1} alignItems="center">
+				<Typography variant="h3" component="h1">
+					Ketcher Tools
+				</Typography>
+				<Typography variant="subtitle1" component="h2">
+					Life science open source EPAM
+				</Typography>
+				<Link href="https://lifescience.opensource.epam.com/ketcher/">
+					EPAM documentation
+				</Link>
+			</Stack>
+			<iframe
+				src="./src/pages/KetcherPage/index.html"
+				style={{ border: "none", height: "100%", width: "100%" }}
+				title="Ketcher-Standalone"
+			></iframe>
+		</Stack>
+	);
 };
 
 export default PageKetcher;
