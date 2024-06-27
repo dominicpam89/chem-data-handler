2cf22e9a5f0fe5f2e562fa6068b73de22cf7a947
dominicpam89 <dominicpam89@gmail.com>
2024-06-27 22:16:42 +0700

    Setup the vitest and jestdom in the project: - vite.config.ts - package.json - setupTests.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,10 @@ export default defineConfig(({ mode }) => {
 		optimizeDeps: {
 			include: ["@emotion/styled"],
 		},
+		test: {
+			globals: true,
+			environment: "jsdom",
+			setupFiles: "./src/setupTests.ts",
+		},
 	};
 });
