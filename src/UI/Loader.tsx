152d8fd4b96412d68ab28c27bf31da2c0419f697
dominicpam89 <dominicpam89@gmail.com>
2024-06-27 22:45:59 +0700

    fix root folder as @

--- src/UI/Loader.tsx
+++ src/UI/Loader.tsx
@@ -1,11 +1,11 @@
-import { Box, LinearProgress } from "@mui/material"
+import { Box, LinearProgress } from "@mui/material";
 
-const UILoader = ()=>{
-  return (
-		<Box sx={{ width: "100%" }}>
-			<LinearProgress />
+const UILoader = () => {
+	return (
+		<Box sx={{ width: "100%" }} aria-label="loader-container">
+			<LinearProgress aria-label="loader" />
 		</Box>
-  )
-}
+	);
+};
 
-export default UILoader
+export default UILoader;
