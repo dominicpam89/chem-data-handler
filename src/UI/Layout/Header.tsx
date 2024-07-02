3c9a4105ae8cd33152db7713314bcfd8b07f7164
dominicpam89 <dominicpam89@gmail.com>
2024-07-02 13:28:53 +0700

    added documentation in UI/Layout/Header.tsx

--- src/UI/Layout/Header.tsx
+++ src/UI/Layout/Header.tsx
@@ -2,6 +2,17 @@ import MobileNavbar from "./Header/MobileNavbar";
 import DekstopNavbar from "./Header/DesktopNavbar";
 import { useEffect, useState } from "react";
 
+/**
+ * Custom hook to determine if the screen width is greater than 768 pixels.
+ * @returns {boolean} - Returns true if the screen width is greater than 768 pixels, otherwise false.
+ * @example
+ * const isDesktop = useScreenSize();
+ * if (isDesktop) {
+ *   // Render desktop components
+ * } else {
+ *   // Render mobile components
+ * }
+ */
 const useScreenSize = () => {
 	const [isDesktop, setIsDesktop] = useState<boolean>(window.innerWidth > 768);
 	const updateScreenWidth = () => {
@@ -14,6 +25,11 @@ const useScreenSize = () => {
 	return isDesktop;
 };
 
+/**
+ * UILayoutHeader component that displays either a mobile or desktop navigation bar
+ * based on the screen width.
+ * @component
+ */
 const UILayoutHeader = () => {
 	const isDesktop = useScreenSize();
 	return (
