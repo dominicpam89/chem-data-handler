569a718023762c17d6ecf7f25df08d2618759ef4
dominicpam89 <dominicpam89@gmail.com>
2024-07-02 13:27:04 +0700

    added documentation in UI/Brand.tsx and UI/Loader.tsx

--- src/UI/Brand.tsx
+++ src/UI/Brand.tsx
@@ -1,53 +1,79 @@
-import { Stack, Typography, styled } from "@mui/material"
-import { BiSolidAnalyse } from "react-icons/bi"
+import { Typography, styled } from "@mui/material";
+import { BiSolidAnalyse } from "react-icons/bi";
 
+/**
+ * A custom styled component using react-icons's icon with inherited color.
+ * This component serves as a base component for other specific brand icons.
+ * @component
+ * @example
+ * const BrandIconExtension = styled(BrandIcon)(() => ({
+ *   fontSize: "20px",
+ *   color: "red"
+ * }));
+ */
 export const BrandIcon = styled(BiSolidAnalyse)(({}) => ({
 	fontSize: "24px",
 	color: "inherit",
-}))
+}));
 
-export const MobileNavbarBrandIcon = styled(BiSolidAnalyse)(({ }) => ({
+/**
+ * A custom styled component using react-icons's icon with inherited color.
+ * Specifically designed for use in mobile navigation bars.
+ * @component
+ */
+export const MobileNavbarBrandIcon = styled(BiSolidAnalyse)(({}) => ({
 	fontSize: "24px",
 	color: "inherit",
-}))
+}));
 
-export const DesktopNavbarBrandIcon = styled(BiSolidAnalyse)(({theme}) => ({
+/**
+ * A custom styled component using react-icons's icon with inherited color.
+ * Specifically designed for use in Desktop Navigation component.
+ * @component
+ */
+export const DesktopNavbarBrandIcon = styled(BiSolidAnalyse)(({ theme }) => ({
 	fontSize: "32px",
 	color: theme.palette.primary.contrastText,
-}))
+}));
 
+/**
+ * A custom styled component using react-icons's icon with inherited color.
+ * Specifically designed for Auth page
+ * @component
+ */
 export const AuthBrandIcon = styled(BiSolidAnalyse)(({ theme }) => ({
 	fontSize: "36px",
 	color: theme.palette.primary.main,
 	[theme.breakpoints.up("md")]: {
 		fontSize: "54px",
 	},
-}))
+}));
 
+/**
+ * A custom styled component using react-icons's icon with inherited color.
+ * Specifically designed for Sidebar navigation in Desktop Navigation component
+ * @component
+ */
 export const SidebarBrandIcon = styled(BiSolidAnalyse)(({ theme }) => ({
 	fontSize: "36px",
 	color: theme.palette.primary.main,
-}))
+}));
 
-interface IBrandText{
-	variant: "h1"|"h2"|"h3"|"h4"|"h5"|"h6"|"body1"|"body2"
-	color: string | any
+/**
+ * Interface for BrandText component props.
+ * @type {Object} IBrandText
+ * @component BrandText
+ * @property {"h1"|"h2"|"h3"|"h4"|"h5"|"h6"|"body1"|"body2"} variant - The typography variant to be used in @mui/material/Typography component.
+ * @property {string|any} color - The color of the text to be used in @mui/material/Typography component.
+ */
+interface IBrandText {
+	variant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "body1" | "body2";
+	color: string | any;
 }
-export const BrandText:React.FC<IBrandText> = ({variant, color})=>{
+export const BrandText: React.FC<IBrandText> = ({ variant, color }) => {
 	return (
-		<Typography component="h1" variant={variant} color={color} >
+		<Typography component="h1" variant={variant} color={color}>
 			{import.meta.env.VITE_APP_NAME}
 		</Typography>
-	)
-}
-
-const UIBrand = () => {
-	return (
-		<Stack direction="row" spacing={1} justifyContent="center" alignItems="center">
-			<BrandIcon />
-			<BrandText color="inherit" variant="h6" />
-		</Stack>
-	)
-}
-
-export default UIBrand
+	);
+};
--- src/UI/Loader.tsx
+++ src/UI/Loader.tsx
@@ -1,5 +1,24 @@
 import { Box, LinearProgress } from "@mui/material";
 
+/**
+ * A UI loader component that displays a linear progress bar.
+ * This component is intended to be displayed when there is asynchronous data that needs to be loaded first.
+ * @component
+ * @example
+ * // Usage in a component
+ * const MyComponent = () => {
+ *   const [loading, setLoading] = useState(true);
+ *   useEffect(() => {
+ *     fetchData().then(() => setLoading(false));
+ *   }, []);
+ *   return (
+ *     <div>
+ *       {loading ? <UILoader /> : <DataDisplay />}
+ *     </div>
+ *   );
+ * };
+ */
+
 const UILoader = () => {
 	return (
 		<Box sx={{ width: "100%" }} aria-label="loader-container">
