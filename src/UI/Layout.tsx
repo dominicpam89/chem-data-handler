ec539ec84b0d62af97f80461d977d10695bc9516
dominicpam89 <dominicpam89@gmail.com>
2024-07-02 13:02:42 +0700

    added documentation for UI/Layout.tsx

--- src/UI/Layout.tsx
+++ src/UI/Layout.tsx
@@ -4,6 +4,22 @@ import UILayoutHeader from "./Layout/Header";
 import UILayoutFooter from "./Layout/Footer";
 import ScrollToTop from "./Layout/ScrollToTop";
 
+/**
+ * The RootLayout component provides the main layout structure for the application.
+ * It includes the header, main content area (with an outlet for nested routes),
+ * and footer, as well as a scroll-to-top functionality.
+ *
+ * @returns {JSX.Element} rendered RootLayout component
+ * @copyright dominicpam89
+ * @example
+ * const router = createBrowserRouter([
+ * 	{path:"/", element: <RootLayout />}
+ * ])
+ * export default function App(){
+ * 	return <RouterProvider router={router} />
+ * }
+ */
+
 const RootLayout = () => {
 	return (
 		<>
